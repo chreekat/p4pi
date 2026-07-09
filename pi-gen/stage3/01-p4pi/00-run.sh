98f50616bb9be1bec80d95c441a2bb5b4bedb211
Bryan Richter <b@chreekat.net>
2026-07-09 20:20:51 +0300

    Add deb sources for other p4lang packages

--- pi-gen/stage3/01-p4pi/00-run.sh
+++ pi-gen/stage3/01-p4pi/00-run.sh
@@ -7,6 +7,9 @@ on_chroot << EOF
 echo 'deb http://download.opensuse.org/repositories/home:/p4lang/Raspbian_11/ /' | tee /etc/apt/sources.list.d/p4pi-kernel.list
 curl -fsSL https://download.opensuse.org/repositories/home:p4lang/Raspbian_11/Release.key | gpg --dearmor > /etc/apt/trusted.gpg.d/home_p4lang.gpg
 
+echo 'deb http://download.opensuse.org/repositories/home:/p4lang/Debian_11/ /' | tee /etc/apt/sources.list.d/home:p4lang.list
+curl -fsSL https://download.opensuse.org/repositories/home:p4lang/Debian_11/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/home_p4lang.gpg > /dev/null
+
 apt-get -y update
 
 wget https://raw.githubusercontent.com/p4lang/behavioral-model/main/tools/p4dbg.py
