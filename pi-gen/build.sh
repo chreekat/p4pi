174e23456946ad8b1525e3db883d1dd52894fa3f
Bryan Richter <b@chreekat.net>
2026-07-09 19:53:40 +0300

    Support running 'act' from a submodule
    I use 'act' to run GitHub workflows locally. I also use submodules for
    all my upstream checkouts. 'act' copies the working tree to a temp dir,
    breaking the link to the "real" git dir. As a consequence, "git
    rev-parse" fails.

--- pi-gen/build.sh
+++ pi-gen/build.sh
@@ -240,7 +240,8 @@ export KEYBOARD_LAYOUT="${KEYBOARD_LAYOUT:-English (UK)}"
 
 export TIMEZONE_DEFAULT="${TIMEZONE_DEFAULT:-Europe/London}"
 
-export GIT_HASH=${GIT_HASH:-"$(git rev-parse HEAD)"}
+: ${GITH_HASH:="$(git rev-parse HEAD)"}
+export GIT_HASH
 
 export PUBKEY_SSH_FIRST_USER
 
